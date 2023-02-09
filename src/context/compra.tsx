6174fe27982982f55aa245fb0598f6ea94a47e03
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-02-08 22:37:51 -0300

    Commit 23

---
+++ src/context/compra.tsx
@@ -0,0 +1,45 @@
+import { Dispatch, FC, SetStateAction, createContext, ReactNode, useState } from "react";
+import { ListaProdutosTypes } from "../utils/listaProdutos"
+
+export type CarrinhoProdutoTypes = ListaProdutosTypes & { 
+  quantidade: number;
+}
+
+export type CompraType =  {
+  carrinho_compras: CarrinhoProdutoTypes[];
+  preco_total: number;
+}
+
+export type PropsCompraContext = { 
+  state: CompraType;
+  setState: Dispatch<SetStateAction<CompraType>>;
+}
+
+export const ValoresIniciais: PropsCompraContext = {
+  state: {
+    carrinho_compras: [],
+    preco_total: 0
+  },
+  setState: () => {},
+};
+
+export const CompraContext = createContext<PropsCompraContext>(ValoresIniciais);
+
+export interface CompraContextProviderProps {
+  children: ReactNode;
+}
+
+export const CompraContextProvider: FC<CompraContextProviderProps> = (props: CompraContextProviderProps) => {
+  const [state, setState] = useState(ValoresIniciais.state);
+
+  return (
+    <CompraContext.Provider
+      value={{
+        state,
+        setState,
+      }}
+    >
+      {props.children}
+    </CompraContext.Provider>
+  );
+};
